b3eddd4cfca0c724bd257d148dee713c52b86882
volhakr <kresiko@gmail.com>
2023-03-08 12:30:38 +0200

    Switch refactored with strategy pattern

---
+++ src/main/java/org/example/data/Employee.java
@@ -0,0 +1,17 @@
+package org.example.data;
+
+public class Employee {
+
+  private String name;
+  private EmployeeType type;
+
+  public Employee(EmployeeType type) {
+    this.type = type;
+  }
+
+  public EmployeeType getType() {
+    return type;
+  }
+}
+
+
